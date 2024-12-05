bab368a92e7350365f3e483d520d01d6e4be5c11
jesusrlv <jesusrlvrojo@gmail.com>
2024-12-04 20:58:13 -0600

    consultas ready

--- js/script.js
+++ js/script.js
@@ -8,3 +8,29 @@ function cargaCategoria(){
         }
     });
 }
+
+function cargaProductosRandom(){
+    $.ajax({
+        type: "POST",
+        url: "prcd/cargarProductosRandom.php",
+        dataType: "html",
+        success: function(data){
+            $('#inventario').fadeIn(1000).html(data);    
+        }
+    });
+}
+
+function cargaProductos(producto){
+    document.getElementById('inventario').innerHTML = "";
+    $.ajax({
+        type: "POST",
+        url: "prcd/cargarProductos.php",
+        data:{
+            producto:producto
+        },
+        dataType: "html",
+        success: function(data){
+            $('#inventario').fadeIn(1000).html(data);    
+        }
+    });
+}
---
+++ prcd/cargarProductos.php
@@ -0,0 +1,31 @@
+<?php
+
+require('../prcd/conn.php');
+
+$producto = $_POST['producto'];
+
+$sql = "SELECT * FROM inventario WHERE categoria = '$producto' ORDER BY id ASC";
+$resultado = $conn->query($sql);
+
+while($row = $resultado->fetch_assoc()){
+    echo '
+    
+    <div class="col-md-3 col-sm-12">
+        <div class="card" style="width: 18rem;height:50vh;">
+        <img src="productos/'.$row['ruta'].'" class="card-img-top" alt="..."  id="imagenCatalogo">
+        <div class="card-body">
+            <h5 class="card-title">
+                <span class="badge text-bg-primary">'.$row['descripcion'].'</span>
+            </h5>
+            <p><span><strong>Precio:</strong></span> $'.$row['precio'].'</p>
+        </div>
+        <div class="card-footer">
+            <div class="d-grid gap-2">
+            <button class="btn btn-primary" type="button"><i class="bi bi-journals"></i> Ver</button>
+            </div>
+        </div>
+        </div>
+    </div>
+
+    ';
+}
