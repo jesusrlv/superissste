93870a13e4ff0238cfaec607d429ed50016ea25e
jesusrlv <jesusrlvrojo@gmail.com>
2024-12-04 20:16:12 -0600

    consultas

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
-function inicio{
-    
+function cargaCategoria(){
+    $.ajax({
+        type: "POST",
+        url: "prcd/cargarCategoria.php",
+        dataType: "html",
+        success: function(data){
+            $('#categoria').fadeIn(1000).html(data);    
+        }
+    });
 }
